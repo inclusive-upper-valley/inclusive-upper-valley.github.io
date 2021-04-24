6f6fc50ccb3e954ee464111d6af6a35707167c44
Richard Adams <204173+richarda@users.noreply.github.com>
2021-04-23 23:28:38 -0400

    initial shell of site

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,14 @@
 import React from "react"
+import Layout from "../components/layout"
+import { StaticImage } from "gatsby-plugin-image"
 
 export default function Home() {
-  return <div>Hello world!</div>
+  return (
+    <Layout>
+      <StaticImage src="../images/logo.jpg" alt="Logo" />
+      <p>
+        Here is some more detail...
+      </p>
+    </Layout>
+  )
 }
